5a2c70e7e464b5b5e95fc773135b3fde187e0257
Brenna Meyer <brennameyer9@gmail.com>
2025-02-28 15:00:31 -0500

    Add project files.

---
+++ Chapter 4 Exercise 1.cpp
@@ -0,0 +1,14 @@
+// Chapter 4 Exercise 1.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//Programmer: Brenna Meyer
+// Date: Feb 28th,2024
+//Requirements: Write a program that asks the user to enter two numbers. 
+// The program should use the conditional operator to determine which number is the smaller and which is the larger
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
+
